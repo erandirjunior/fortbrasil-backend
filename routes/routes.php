2a987ce0782445d5b07828c96e235eb31c4d9487
Erandir Junior <aefs12junior@gmail.com>
2020-09-24 16:29:19 -0300

    Created new routes

--- routes/routes.php
+++ routes/routes.php
@@ -10,10 +10,24 @@ $route->options('/{anything}', function () {
     return '';
 });
 
+$route->post('login', '\\User\\Login@execute');
+$route->post('register', '\\User\\Create@execute');
+
 $route->group(['prefix' => '/', 'namespace' => 'SRC\Infrastructure\Api'], function ($route) {
-    $route->post('users', '\\User\\Create@execute');
-    $route->put('users', '\\User\\Update@execute');
-    $route->get('users/{id:\d+}', '\\User\\Find@execute');
+    $route->group(['prefix' => 'users'], function ($route) {
+        $route->get('/{id:\d+}', '\\User\\Find@execute');
+        $route->put('/{id:\d+}', '\\User\\Update@execute');
+        $route->delete('/{id:\d+}', '\\User\\Delete@execute');
+    });
+
+    $route->group(['prefix' => 'establishments'], function ($route) {
+        $route->post('', '\\User\\Create@execute');
+        $route->get('/names', '\\User\\Find@execute');
+        $route->get('/names', '\\User\\Find@execute');
+        $route->put('/{id:\d+}', '\\User\\Update@execute');
+        $route->delete('/{id:\d+}', '\\User\\Delete@execute');
+    });
+
     $route->get('establishments', '\\Establishment@create');
 });
 
