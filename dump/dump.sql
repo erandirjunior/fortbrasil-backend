57a087b35156150928edfded1652d3c9b8e660db
Erandir <aefs12junior@gmail.com>
2020-09-28 15:08:37 -0300

    Removed columns

--- dump/dump.sql
+++ dump/dump.sql
@@ -2,24 +2,22 @@ CREATE DATABASE IF NOT EXISTS fortbrasil;
 
 use fortbrasil;
 
-CREATE TABLE IF NO EXISTS establishment
+CREATE TABLE IF NOT EXISTS establishment
 (
     id         INT AUTO_INCREMENT
         PRIMARY KEY,
     name       VARCHAR(255)                       NOT NULL,
-    address    VARCHAR(255)                       NULL,
     zip_code   VARCHAR(8)                         NULL,
     number     INT                                NULL,
     street     VARCHAR(255)                       NULL,
     city       VARCHAR(255)                       NULL,
     state      VARCHAR(255)                       NULL,
-    complement TEXT                               NULL,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP NULL,
     deleted_at DATETIME                           NULL,
     updated_at DATETIME                           NULL
 );
 
-CREATE TABLE IF NO EXISTS establishment_phone
+CREATE TABLE IF NOT EXISTS contact
 (
     id               INT AUTO_INCREMENT
         PRIMARY KEY,
@@ -29,11 +27,11 @@ CREATE TABLE IF NO EXISTS establishment_phone
     deleted_at       DATETIME                                  NULL,
     updated_at       DATETIME                                  NULL,
     type             ENUM ('1', '2') DEFAULT '1'               NULL,
-    CONSTRAINT establishment_phone_establishment_id_fk
+    CONSTRAINT contact_establishment_id_fk
         FOREIGN KEY (establishment_id) REFERENCES establishment (id)
 );
 
-CREATE TABLE IF NO EXISTS user
+CREATE TABLE IF NOT EXISTS user
 (
     id         INT AUTO_INCREMENT
         PRIMARY KEY,
